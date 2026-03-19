b4b4177a5b9e261ccbfd142712a82f8ca5780be3
Ale Dangio <aledangio@gmail.com>
2026-03-19 19:29:00 -0300

    primeira automação usando IA

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -6,35 +6,172 @@ import { generateOrderCode } from './support/helpers'
 
 test.describe('Consulta de Pedido', () => {
 
-  test.beforeEach(async ({page})=>{
+  test.beforeEach(async ({ page }) => {
     //Arrange
     await page.goto('http://localhost:5173/')
     await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
     await page.getByRole('link', { name: 'Consultar Pedido' }).click()
     await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-  })  
+  })
 
   test('deve consultar um pedido aprovado', async ({ page }) => {
 
     //Test Data
-    const order = 'VLO-AZ1H0G'
+    //const order = 'VLO-AZ1H0G'
+
+    const order = {
+      number: 'VLO-AZ1H0G',
+      status: 'APROVADO',
+      color: 'Midnight Black',
+      wheels: 'aero Wheels',
+      customer: {
+        name: 'DANIELA DE OLIVEIRA',
+        email: 'danirabelo81@gmail.com',
+      },
+      payment: 'À Vista',
+    }
 
     //Act
-    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
     await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
     //Assert
-
-    const containerPedido = page.getByRole('paragraph')
-      .filter({ hasText: /^Pedido$/ })
-      .locator('..')  //Sobe para o elemento pai
-
-    await expect(containerPedido).toContainText(order, { timeout: 10000 })
-
-    await expect(page.getByText('APROVADO')).toBeVisible()
-
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+  - img
+  - paragraph: Pedido
+  - paragraph: ${order.number}
+  - img
+  - text: ${order.status}
+  - img "Velô Sprint"
+  - paragraph: Modelo
+  - paragraph: Velô Sprint
+  - paragraph: Cor
+  - paragraph: ${order.color}
+  - paragraph: Interior
+  - paragraph: cream
+  - paragraph: Rodas
+  - paragraph: ${order.wheels}
+  - heading "Dados do Cliente" [level=4]
+  - paragraph: Nome
+  - paragraph: ${order.customer.name}
+  - paragraph: Email
+  - paragraph: ${order.customer.email}
+  - paragraph: Loja de Retirada
+  - paragraph
+  - paragraph: Data do Pedido
+  - paragraph: /\\d+\\/\\d+\\/\\d+/
+  - heading "Pagamento" [level=4]
+  - paragraph: ${order.payment}
+  - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+  `)
   })
+
+  test('deve consultar um pedido reprovado', async ({ page }) => {
+
+    //Test Data
+    //const order = 'VLO-5OM1CX'
+    const order = {
+      number: 'VLO-5OM1CX',
+      status: 'REPROVADO',
+      color: 'Midnight Black',
+      wheels: 'sport Wheels',
+      customer: {
+        name: 'Steve Jobs',
+        email: 'jobs@apple.com',
+      },
+      payment: 'À Vista',
+    }
+
+      //Act
+      await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+      await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+
+    //Assert
+
+await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+  - img
+  - paragraph: Pedido
+  - paragraph: ${order.number}
+  - img
+  - text: ${order.status}
+  - img "Velô Sprint"
+  - paragraph: Modelo
+  - paragraph: Velô Sprint
+  - paragraph: Cor
+  - paragraph: ${order.color}
+  - paragraph: Interior
+  - paragraph: cream
+  - paragraph: Rodas
+  - paragraph: ${order.wheels}
+  - heading "Dados do Cliente" [level=4]
+  - paragraph: Nome
+  - paragraph: ${order.customer.name}
+  - paragraph: Email
+  - paragraph: ${order.customer.email}
+  - paragraph: Loja de Retirada
+  - paragraph
+  - paragraph: Data do Pedido
+  - paragraph: /\\d+\\/\\d+\\/\\d+/
+  - heading "Pagamento" [level=4]
+  - paragraph: ${order.payment}
+  - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+  `)
+
+    })
+
+    test('deve consultar um pedido em analise', async ({ page }) => {
+
+      //Test Data
+      //const order = 'VLO-5OM1CX'
+      const order = {
+        number: 'VLO-N17UAH',
+        status: 'EM_ANALISE',
+        color: 'Lunar White',
+        wheels: 'aero Wheels',
+        customer: {
+          name: 'João da Silva',
+          email: 'joao@velo.dev',
+        },
+        payment: 'À Vista',
+      }
+  
+        //Act
+        await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+        await page.getByRole('button', { name: 'Buscar Pedido' }).click()
+  
+      //Assert
+  
+  await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+    - img
+    - paragraph: Pedido
+    - paragraph: ${order.number}
+    - img
+    - text: ${order.status}
+    - img "Velô Sprint"
+    - paragraph: Modelo
+    - paragraph: Velô Sprint
+    - paragraph: Cor
+    - paragraph: ${order.color}
+    - paragraph: Interior
+    - paragraph: cream
+    - paragraph: Rodas
+    - paragraph: ${order.wheels}
+    - heading "Dados do Cliente" [level=4]
+    - paragraph: Nome
+    - paragraph: ${order.customer.name}
+    - paragraph: Email
+    - paragraph: ${order.customer.email}
+    - paragraph: Loja de Retirada
+    - paragraph
+    - paragraph: Data do Pedido
+    - paragraph: /\\d+\\/\\d+\\/\\d+/
+    - heading "Pagamento" [level=4]
+    - paragraph: ${order.payment}
+    - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+    `)
+  
+      })  
 
   test('deve exibir mensagem quando o pedido não é encontrado', async ({ page }) => {
 
